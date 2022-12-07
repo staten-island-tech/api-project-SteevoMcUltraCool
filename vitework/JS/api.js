9a368ea54f6cb7f42f1e5dbb53f8ace47b6ceee1
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2022-12-06 19:08:07 -0500

    Create api.js

---
+++ vitework/JS/api.js
@@ -0,0 +1,41 @@
+
+async function getJoke() {
+    try {
+        let joke = await fetch("https://icanhazdadjoke.com/",{
+            method: 'GET', 
+            headers: {
+              'Accept': 'application/json ',
+            }
+        })
+        return (await joke.json()).joke
+    }catch(er) {
+        console.log(er)
+    }
+}
+async function getCompatability(name,lover) {
+    try {
+        let compatibility = await fetch(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${lover}&fname=${name}`,{
+            method: 'GET', 
+            headers: {
+                'X-RapidAPI-Key': '82720bd7d6msh2d19c07fd7db3ddp1297f3jsn24bf33fef3b4',
+                'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'            }
+        })
+        let comp = (await compatibility.json())
+        return [comp.percentage, comp.result]
+    }catch(er) {
+        console.log(er)
+    }
+}
+async function getQuote() {
+    try {
+        let compatibility = await fetch("https://api.quotable.io/random")
+        let comp = (await compatibility.json())
+        return [comp.content, comp.author]
+    }catch(er) {
+        console.log(er)
+    }
+}
+console.log("a new beginning")
+console.log(await getQuote())
+
+
