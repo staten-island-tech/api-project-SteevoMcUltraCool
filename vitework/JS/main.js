3f7cea5ddb16a466fb0ba7fde20c74365e47ad2d
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2022-12-08 17:12:42 -0500

    sss

--- vitework/JS/main.js
+++ vitework/JS/main.js
@@ -7,7 +7,11 @@ const DOM = {
 let mousePos = { X: 0, Y: 0 };
 let time = new Date().getTime();
 let mommySize = { X: 200, Y: 0 };
+let charsize = 0
 let speed = mommySize.X / 3.5;
+function M(n,d) {
+  return Math.max(0, Math.min(mommySize[d] - charsize,n))
+}
 setInterval(function () {
   let newTime = new Date().getTime();
   let deltaTime = (newTime - time) / 1000;
@@ -21,16 +25,16 @@ setInterval(function () {
   };
   let deltaX = mousePos.X - currentPos.X;
   let deltaY = mousePos.Y - currentPos.Y;
-  if (Math.abs(deltaX + deltaY) >= speed / 32) {
+  if (Math.sqrt(deltaX**2 + deltaY**2) >= speed / 65) {
     let data = Math.atan(deltaY / deltaX);
     deltaX = Math.cos(data) * speed * Math.sign(deltaX) * deltaTime;
     deltaY = Math.sin(data) * speed * Math.sign(deltaX) * deltaTime;
     console.log(speed);
-    DOM.root.style.setProperty("--Xpos", `${currentPos.X + deltaX}px`);
-    DOM.root.style.setProperty("--Ypos", `${currentPos.Y + deltaY}px`);
+    DOM.root.style.setProperty("--Xpos", `${M(currentPos.X + deltaX,"X")}px`);
+    DOM.root.style.setProperty("--Ypos", `${M(currentPos.Y + deltaY,"Y")}px`);
   } else {
-    DOM.root.style.setProperty("--Xpos", `${mousePos.X}px`);
-    DOM.root.style.setProperty("--Ypos", `${mousePos.Y}px`);
+    DOM.root.style.setProperty("--Xpos", `${M(mousePos.X,"X")}px`);
+    DOM.root.style.setProperty("--Ypos", `${M(mousePos.Y,"Y")}px`);
   }
 }, 33);
 
@@ -48,6 +52,7 @@ function newSize() {
     X: Number(mamaStuff.right) - Number(mamaStuff.left),
     Y: Number(mamaStuff.bottom) - Number(mamaStuff.top),
   };
+  charsize = mommySize.X/20
   speed = mommySize.X / 3.5;
 }
 window.addEventListener("resize", newSize);
