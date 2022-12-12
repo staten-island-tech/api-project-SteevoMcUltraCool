4bd337cd93c9661201d2934fc7d28106ac0c8ad3
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2022-12-12 10:17:22 -0500

    Create prompt.js

---
+++ vitework/JS/prompt.js
@@ -0,0 +1,75 @@
+let promptArray = []
+
+class p { 
+  constructor(DOMME, header, query){
+    this.element = DOMME
+    this.state = "disabled"
+    this.display = DOMME.style
+    this.display.display = "none"
+    this.element.class = "prompt"
+    this.headerElement = document.createElement("h1")
+    this.textElement = document.createElement("p")
+    this.headerElement.innerHTML = header
+    this.element.appendChild(this.headerElement)
+    this.element.appendChild(this.textElement)
+    this.promptText = query
+    promptArray.push(this)
+  }
+  setProcedure(typeProcedure, procedure){
+    if (typeProcedure == "initiate"){
+       this.initiateProcedure = procedure
+    }else if (typeProcedure=="accepted"){
+      this.acceptedProcedure = procedure
+    }else if (typeProcedure=="denied"){
+      this.deniedProcedure = procedure 
+    }else if(typeProcedure=="reset"){
+      this.resetProcedure = procedure
+    }else if (typeProcedure=="setDefault"){
+      let values = procedure()
+      this.initiateProcedure = function(){
+        this.state = "pendingInitiation"
+        this.display.display = "inline"
+        this.displayText(values[1]|| values.initiateText,0.033, true)
+        this.state = "initiated"
+      }
+      this.acceptedProcedure = function(){
+        this.state= "pendingAccepted"
+        this.displayText(values[2] || values.acceptedText, 0.033, true)
+        this.state = "accepted"
+      }
+      this.deniedProcedure = function(){
+        this.state= "pendingDenied"
+        this.displayText(values[3] || values.deniedText, 0.033, true)
+        this.state = "denied"        
+      }
+      this.resetProcedure = function(){
+        this.state = "pendingReset"
+        this.display.display = "none"
+        this.state = "off"
+      }
+      this.state="off"
+    }
+  }
+  displayText(string, speed, erase){
+    speed = speed || 0.033
+    if (erase){
+      this.textElement.innerHTML = ""
+    }
+    let sum = string.length
+    let count = 0
+    do {
+      count=count+1
+      setTimeout(function(){
+        if (this.state !="off"){
+          this.textElement.innerHTML = string.sub(0,count)
+       }else {
+        count = sum + 100
+       }
+      },count*speed*1000)
+    }while(count<sum)
+  }
+}
+  
+
+
+export let prompt = p
